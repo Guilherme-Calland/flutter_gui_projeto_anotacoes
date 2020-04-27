9b823dd4ef043a514b83edf79878a4f9d456cd0b
guilhermow <gui.crb0@gmail.com>
2020-04-27 20:24:34 -0300

    tela de dialogo

--- lib/Home.dart
+++ lib/Home.dart
@@ -6,6 +6,66 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  TextEditingController _controladorTitulo = TextEditingController();
+  TextEditingController _controladorDescricao = TextEditingController();
+
+  _mostrarTelaDialogo(){
+    showDialog(
+      context: context,
+      builder: (context){
+        return AlertDialog(
+          title: Text('salvar anotacao'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'titulo',
+                  hintText: 'digite um titulo'
+                ),
+                controller: _controladorTitulo,
+                autofocus: true
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'descricao',
+                  hintText: 'digite uma descricao'
+                ),
+                controller: _controladorDescricao
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            RaisedButton(
+              child: Text(
+                'cancelar',
+                style: TextStyle(
+                  color: Colors.red
+                )
+              ),
+              onPressed: (){
+                return Navigator.pop(context);
+              }
+            ),
+            RaisedButton(
+              child: Text(
+                'salvar',
+                style: TextStyle(
+                  color: Colors.green
+                )
+              ),
+              onPressed: (){
+                // salvar
+                return Navigator.pop(context);
+              },
+            )
+          ],
+        );
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -19,7 +79,9 @@ class _HomeState extends State<Home> {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         backgroundColor: corTema,
-        onPressed: (){},
+        onPressed: (){
+          _mostrarTelaDialogo();
+        },
       ),
     );
   }
